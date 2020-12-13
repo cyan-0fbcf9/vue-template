330f59564ca4a0c27b5a5a5dc21f846d2b99bc4f
Cyan-on-win <namid224y@gmail.com>
2020-12-13 14:52:05 +0900

    configure base

---
+++ vue.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  chainWebpack: config => {
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal'];
+    types.forEach(type =>
+      addStyleResource(config.module.rule('scss').oneOf(type))
+    );
+  }
+};
+
+function addStyleResource(rule) {
+  rule
+    .use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/styles/scss/variables.scss'),
+        path.resolve(__dirname, './src/styles/scss/colors.scss'),
+        path.resolve(__dirname, './src/styles/scss/imports.scss'),
+        path.resolve(__dirname, './src/styles/scss/mixins.scss')
+      ]
+    });
+}
